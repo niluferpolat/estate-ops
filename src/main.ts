d7b74c69abf07e517c4de1709cd8afe26b0be4a2
nilufer polat <niluferpolat2@gmail.com>
2025-12-03 15:47:44 +0300

    added global error

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
+import { ValidationError } from 'class-validator';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+      exceptionFactory: (errors: ValidationError[] = []) => {
+        return new BadRequestException({
+          message: 'Fill required fields',
+          errors: errors.map(err => ({
+            field: err.property,
+            constraints: err.constraints,
+          })),
+        });
+      },
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
