10c4b5a8a5dcebba37fa4119b9d2e7ca8cc897a0
nilufer polat <niluferpolat2@gmail.com>
2025-12-04 00:33:33 +0300

    swagger implementation

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { BadRequestException, ValidationPipe } from '@nestjs/common';
 import { ValidationError } from 'class-validator';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -21,6 +22,10 @@ async function bootstrap() {
       },
     }),
   );
+  const config = new DocumentBuilder().setTitle('Estate OPS API').setDescription('Real Estate Operations REST API Documentation').setVersion('1.0').build();
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
