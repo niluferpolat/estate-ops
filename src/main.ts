d5d291db8939708542076b6c24e243680710ded2
nilufer polat <niluferpolat2@gmail.com>
2025-12-04 01:44:25 +0300

    edited main

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,9 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -22,10 +25,14 @@ async function bootstrap() {
       },
     }),
   );
-  const config = new DocumentBuilder().setTitle('Estate OPS API').setDescription('Real Estate Operations REST API Documentation').setVersion('1.0').build();
-  const document = SwaggerModule.createDocument(app, config);
+  const config = new DocumentBuilder().setTitle('Estate OPS API').setDescription('Real Estate Operations REST API Documentation').setVersion('1.0').addBearerAuth().build();
 
+  const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
+
+  console.log(`🚀 App running on port ${port}`);
 }
+
 bootstrap();
